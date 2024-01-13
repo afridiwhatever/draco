7c959ebb427960102aae545ab0cd953ac7f36630
Afridi <afridiwhatever@gmail.com>
2024-01-14 02:02:23 +0600

    navar responsiveness complete

--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 
 const { log } = console;
 
-const navbar = document.querySelector("[data-navbar]");
-const navToggler = document.querySelector("[data-nav-toggler]");
+function addEventOnElements(elements, event, callback) {
+  elements.forEach((el) => {
+    el.addEventListener(event, callback);
+  });
+}
 
-navToggler.addEventListener("click", () => {
+function toggleNavbar() {
   navbar.classList.toggle("active");
-});
+  header.classList.toggle("nav-active");
+}
+
+const header = document.querySelector("[data-header-container]");
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = Array.from(document.querySelectorAll("[data-nav-toggler]"));
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
